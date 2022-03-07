a9ab8648a15c3a0f5d5709a0dc3d8ce55d7002fa
Opilane TTHK <opilane@tthk.ee>
2022-03-07 14:54:15 +0200

    Add project files.

---
+++ TARgv20/Box_Page.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv20
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Box_Page : ContentPage
+    {
+        BoxView box;
+        Random rnd;
+        public Box_Page()
+        {
+            box = new BoxView {
+                Color = Color.FromRgb(45, 250, 150),
+                CornerRadius = 20,
+                WidthRequest = 100,
+                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                VerticalOptions=LayoutOptions.FillAndExpand
+            };
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            box.GestureRecognizers.Add(tap);
+
+            StackLayout st = new StackLayout { Children = { box } };
+            Content = st;
+        }
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            rnd = new Random();
+            box.Color = Color.FromRgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            box.Rotation += 25;
+            try
+            {
+                Vibration.Vibrate();
+                var dur = TimeSpan.FromSeconds(0.3);
+                Vibration.Vibrate(dur);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ TARgv20/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TARgv20
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Button box_btn = new Button
+            {
+                Text = "BoxView",
+                BackgroundColor = Color.Azure
+            };
+            box_btn.Clicked += Box_btn_Clicked;
+
+            Button entry_btn = new Button
+            {
+                Text = "Entry",
+                BackgroundColor = Color.Azure
+            };
+            entry_btn.Clicked += Entry_btn_Clicked;
+
+            StackLayout st = new StackLayout
+            {
+                Children = { box_btn, entry_btn }
+            };
+            st.BackgroundColor = Color.Beige;
+            Content = st;
+        }
+
+        private async void Entry_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Entry_Page());
+        }
+
+        private async void Box_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Box_Page());
+        }
+    }
+}
