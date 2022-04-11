e997be5e3695661c26c3d5fa3fb548edae2d226c
MastroPhaedra <zakharmakar1@gmail.com>
2022-04-11 21:25:54 +0300

    + Maakonnad_Page, Horoskop_Page, Ajaplaan_Page

---
+++ TARgv20/Horoskop_Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARgv20
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Horoskop_Page : ContentPage
+    {
+        public Horoskop_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
